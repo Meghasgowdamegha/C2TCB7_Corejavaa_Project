f472ccb5d948e2406bba1ccb976b2c6516660fb1
Meghasgowdamegha <96436236+Meghasgowdamegha@users.noreply.github.com>
2022-01-20 19:25:31 +0530

    First commit

---
+++ src/hash_set_20_Jan/HashSetDemo.java
@@ -0,0 +1,27 @@
+package hash_set_20_Jan;
+
+	import java.util.HashSet;
+	import java.util.Iterator;
+	import java.util.TreeSet;
+
+	public class HashSetDemo {
+
+		public static void main(String[] args) {
+			TreeSet<String> hs=new TreeSet<String>();
+	        hs.add("Hash");
+	        hs.add("Set");
+	        hs.add("Demo");
+	        hs.add("Demo");
+	        //hs.add(new Integer(10));
+		   
+	       Iterator i=hs.iterator();
+	       while(i.hasNext())
+	       {
+	    	   Object o=i.next();
+	    	   System.out.println(o);
+	       }
+		}
+
+	}
+
+	
